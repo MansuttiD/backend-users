c808fdaa1e32dba3aa585335bce62fc2a67ef48c
MansuttiD <daniel.mansutti@gmail.com>
2022-11-29 17:39:17 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
 const express = require('express')
 
+const db = require('./utils/database')
+
+const app = express();
+
 const port = 9000
 
-const app = express
+const userRouter = require('./users/users.router')
+
+db.authenticate()
+    .then(() => {
+        console.log('Database Authenticated!');
+    })
+    .catch((err) => {
+        console.log(er);
+    })
+
+    db.sync()
+    .then(()=> {
+        console.log('Database Synced');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Ok!'})
 }) 
 
-app.listen(() => {
+app.use('/api/v1', userRouter)
+
+app.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,12 @@
+const {Sequelize} = require('sequelize')
 
+const db = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'users_db'
+})
 
+module.exports = db
